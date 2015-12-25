52ad5ff41da9cb583a63a35190caf9f021164ed7
Michel Richard <mrichard4@lm3000026417.home>
2015-12-24 21:58:29 -0500

    linting

--- tests.webpack.js
+++ tests.webpack.js
@@ -2,5 +2,5 @@ const testContext = require.context('./src', true, /__tests/);
 testContext.keys().forEach(testContext);
 
 // Want ceverage on files with no tests
-const srcContext = require.context('./src', true, /^(?:(?!__tests).)+$/)
+const srcContext = require.context('./src', true, /^(?:(?!__tests).)+$/);
 srcContext.keys().forEach(srcContext);
